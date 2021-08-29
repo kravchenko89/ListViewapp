5aa0e45d44c1a484b14c050ae24e2624c1361941
KravchenkoVadim <symkin2007@gmail.com>
2021-08-29 20:49:12 +0300

    simple_list_view

--- app/src/main/java/ru/kravchenko/listviewapp/MainActivity.kt
+++ app/src/main/java/ru/kravchenko/listviewapp/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.kravchenko.listviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val array = arrayOf("1", "2", "3", "4", "5", "5", "6",
+            "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
+
+        val list_view: ListView = findViewById(R.id.list_view)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array)
+        list_view.adapter = adapter
+
     }
 }
